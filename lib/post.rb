a17a581e77020a53308037f5b9d89d90fefe155a
Caleb Aviles <earthctzn1@gmail.com>
2019-05-21 02:54:19 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,18 @@
 class Post 
-  attr_accessor :title, :author 
-
-  def initialize(title)
-    @title = title
-    
+attr_accessor :name, :author
+@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if @author
+      @author.name
+    end
   end
 end
