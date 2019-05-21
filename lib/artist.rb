91ad7a6d47069d554fda95a94e43ac993a20da6a
Caleb Aviles <earthctzn1@gmail.com>
2019-05-21 02:06:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,4 +11,14 @@ class Artist
     song.artist = self
   end
   
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+  
+  def self.song_count
+    @songs.count 
+  end
+    
 end 
